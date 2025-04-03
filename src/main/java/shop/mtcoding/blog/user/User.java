86d284f630ed94fb04c1f2cc119e1cb433bfc81b
getinthere <getinthere@naver.com>
2025-04-03 16:43:57 +0900

    이전 방식 회원가입 로그인

---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Table(name = "user_tb")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Builder
+    public User(Integer id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
