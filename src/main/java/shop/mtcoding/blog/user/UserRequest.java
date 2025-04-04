3c6acb28f6cf03be92add267cc32e384d1010211
getinthere <getinthere@naver.com>
2025-04-04 10:25:58 +0900

    인증 로직 완료

--- src/main/java/shop/mtcoding/blog/user/UserRequest.java
+++ src/main/java/shop/mtcoding/blog/user/UserRequest.java
@@ -3,12 +3,21 @@ package shop.mtcoding.blog.user;
 import lombok.Data;
 
 public class UserRequest {
- 
+
+    // insert 용도의 dto에는 toEntity 메서드를 만든다.
     @Data
     public static class JoinDTO {
         private String username;
         private String password;
         private String email;
+
+        public User toEntity() {
+            return User.builder()
+                    .username(username)
+                    .password(password)
+                    .email(email)
+                    .build();
+        }
     }
 
     @Data
